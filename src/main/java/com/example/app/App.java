5a363371cb708932cc2ded9cc5238dc134e89b55
sadiyagit <41662523+sadiyagit@users.noreply.github.com>
2025-07-10 09:53:18 +0530

    Create App.java

---
+++ src/main/java/com/example/app/App.java
@@ -0,0 +1,11 @@
+package com.example.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven App!");
+    }
+
+    public String getGreeting() {
+        return "Hello from Maven App!";
+    }
+}
